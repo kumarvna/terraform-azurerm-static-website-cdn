a58bbc7b67fb43cfd2f60e128af859a51ae44d05
Gabriel GUILLON <gabriel.guillon.p@citeo.com>
2022-07-19 12:05:05 +0200

    Latester version of Az.Cdn
    And few bug fixes

--- Setup-AzCdnCustomDomain.ps1
+++ Setup-AzCdnCustomDomain.ps1
@@ -9,12 +9,13 @@ $azCdnCustomDomainName = $env:FRIENDLY_NAME
 
 try {
   Write-Host 'Checking for existing custom domain name...'
-  $azCustomDomain = Get-AzCdnCustomDomain -CustomDomainName $azCdnCustomDomainName -CdnEndpoint $endpoint -ErrorAction stop
+  $azCustomDomain = Get-AzCdnCustomDomain -CustomDomainName $azCdnCustomDomainName -EndpointName $endpoint.Name -ProfileName $cdnProfile.Name -ResourceGroupName $env:RG_NAME -ErrorAction stop
 }
 catch {
   try {
+    Write-Warning "At this point, you should have made a CNAME record like '$($env:CUSTOM_DOMAIN) IN CNAME $($endpoint.HostName).' Things WILL fail if you did not do that. In that case, please make this record, then relaunch me."
     Write-Host "Enabling custom domain $env:CUSTOM_DOMAIN..."
-    $azCustomDomain = New-AzCdnCustomDomain -HostName $env:CUSTOM_DOMAIN -CdnEndpoint $endpoint -CustomDomainName $azCdnCustomDomainName -ErrorAction stop
+    $azCustomDomain = New-AzCdnCustomDomain -HostName $env:CUSTOM_DOMAIN -EndpointName $endpoint.Name -CustomDomainName $azCdnCustomDomainName -ProfileName $cdnProfile.Name -ResourceGroupName $env:RG_NAME -ErrorAction stop
     continue;
   }
   catch {
@@ -26,13 +27,16 @@ catch {
 if ($azCustomDomain.CustomHttpsProvisioningState -ne 'Enabled' -and $azCustomDomain.CustomHttpsProvisioningState -ne 'Enabling') {
   try {
     Write-Host "Enabling HTTPS for $env:CUSTOM_DOMAIN..."
-    Enable-AzCdnCustomDomainHttps -ResourceId $azCustomDomain.Id -ErrorAction stop
+    # -ProtocolType should be either ServerNameIndication or IPBased
+    # I really don't know what's the difference...
+    $azCustomDomainHttpsParameters = New-AzCdnManagedHttpsParametersObject -CertificateSourceParameterCertificateType Dedicated -CertificateSource Cdn  -ProtocolType ServerNameIndication
+    $azCustomDomainHttps = Enable-AzCdnCustomDomainCustomHttps -ProfileName $cdnProfile.Name -EndpointName $endpoint.Name -CustomDomainName $azCdnCustomDomainName -CustomDomainHttpsParameter $azCustomDomainHttpsParameters -ResourceGroupName $env:RG_NAME -ErrorAction stop
+    Write-Host -Foreground Green "HTTPS enabled for $($env:CUSTOM_DOMAIN). Process can be quite long to terminate (minutes to sometimes hours). Check the Azure page of your custom domain."
   }
   catch {
-    Write-Error "Error enabling HTTPS for $env:CUSTOM_DOMAIN..."
+    Write-Error "Error enabling HTTPS for $($env:CUSTOM_DOMAIN)..."
     throw;
   }
+} else {
+  Write-Warning "Azure is already beeing configuring HTTPS for your custom domain $($env:CUSTOM_DOMAIN)"
 }
-
-
-Write-Host "Success:  CDN configured for HTTPS at $env:CUSTOM_DOMAIN" -ForegroundColor Green
